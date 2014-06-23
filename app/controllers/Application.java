0f0ee24e247535851192788890cb22751b89db2d
meugen <meugen@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2014-06-23 20:03:46 +0000

    Implemented get horoscopes
    git-svn-id: https://svn.meugen.in.ua/Projects/java/J2EE/horoscopes/trunk@279 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,39 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import play.db.DB;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
-import views.html.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result main() {
+        try {
+            final Connection connection = DB.getConnection();
+            final PreparedStatement statement = connection.prepareStatement("SELECT name FROM horo_names_v2 where sex=? order by upname");
+            statement.setInt(1, 1);
+            final ResultSet result = statement.executeQuery();
+
+            final StringBuilder builder = new StringBuilder();
+            while (result.next()) {
+                if (builder.length() > 0) {
+                    builder.append(", ");
+                }
+                builder.append(result.getString(1));
+            }
+            return ok(builder.toString());
+        } catch (SQLException e) {
+            return internalServerError(e.getMessage());
+        }
     }
 
 }

1cbd14e767c7e53905af80962a17a65b8800a063
meugen <meugen@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2014-10-22 17:56:07 +0000

    some changes
    git-svn-id: https://svn.meugen.in.ua/Projects/java/J2EE/horoscopes/trunk@347 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,42 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import helpers.DatabaseHelper;
+import net.pushover.client.PushoverMessage;
+import net.pushover.client.PushoverRestClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import play.db.DB;
+import play.i18n.Messages;
+import play.libs.F;
+import play.libs.Json;
+import play.libs.ws.WS;
+import play.libs.ws.WSResponse;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Application extends Controller {
+
+    private static final Log LOG = LogFactory.getLog(Application.class);
+
+    private static final ExecutorService SERVICE = Executors.newSingleThreadScheduledExecutor();
+    private static final String USER_KEY = "uY88LgsdcrA9kYCMDBLYNNpGmijPuf";
+    private static final String APP_TOKEN = "ah7JwRJZ5CY8a4VtfKUkpttUA6kCRH";
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
@@ -43,4 +68,32 @@ public class Application extends Controller {
         return ok(builder.toString());
     }
 
+    // http://192.168.0.100:5984/acra-myapp/_design/acra-storage/_update/report
+    @BodyParser.Of(BodyParser.Json.class)
+    public static F.Promise<Result> crash() {
+        SERVICE.execute(new PushoverRunnable());
+        return WS.url("http://127.0.0.1:5984/acra-horoscopes/_design/acra-storage/_update/report")
+                .put(request().body().asJson()).map(new F.Function<WSResponse, Result>() {
+                    @Override
+                    public Result apply(WSResponse wsResponse) throws Throwable {
+                        return ok(wsResponse.getBody());
+                    }
+                });
+    }
+
+    public static final class PushoverRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                final PushoverMessage pushoverMessage = PushoverMessage.builderWithApiToken(APP_TOKEN)
+                        .setUserId(USER_KEY)
+                        .setMessage(Messages.get("got.new.error.message"))
+                        .build();
+                new PushoverRestClient().pushMessage(pushoverMessage);
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
+    }
 }

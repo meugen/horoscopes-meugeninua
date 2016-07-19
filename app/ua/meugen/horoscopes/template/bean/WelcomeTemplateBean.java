2110f5ed62d02b6c338f8e8b691c371f85fd9787
meugen <meugen@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2016-07-19 10:19:04 +0000

    Fix
    git-svn-id: https://svn.meugen.in.ua/Projects/java/J2EE/horoscopes/trunk@652 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

--- app/ua/meugen/horoscopes/template/bean/WelcomeTemplateBean.java
+++ app/ua/meugen/horoscopes/template/bean/WelcomeTemplateBean.java
@@ -6,16 +6,6 @@ import java.util.List;
 @Singleton
 public final class WelcomeTemplateBean extends BaseTemplateBean {
 
-    @Override
-    protected void fetchCss(List<String> cssList) {
-        // No css
-    }
-
-    @Override
-    protected void fetchJs(List<String> jsList) {
-        // No js
-    }
-
     public String message() {
         return "Welcome to Play";
     }

976d9ccddafddef37f19c21f5df6c07d0dd39e9f
Yevgeniy Malyshev <yevhen.malyshev@sixt.com>
2019-12-26 23:32:14 +0200

    Adde environments into postgres container

--- build-docker.sh
+++ build-docker.sh
@@ -7,5 +7,5 @@ sudo docker rm -f meugeninua.horoscopes
 sudo docker rm -f meugeninua.postgres
 sudo docker build -t meugeninua.horoscopes:1.0 --force-rm .
 
-sudo docker container run --name meugeninua.postgres --mount "type=bind,src=/var/postgres/data,dst=/var/lib/postgresql/data" --network horoscopes-network -d postgres:9.6
+sudo docker container run --name meugeninua.postgres -e POSTGRES_PASSWORD=horoscopes -e POSTGRRESS_USER=horoscopes -e POSTGRESS_DB=horoscopes --mount "type=bind,src=/var/postgres/data,dst=/var/lib/postgresql/data" --network horoscopes-network -d postgres:9.6
 sudo docker container run --name meugeninua.horoscopes --network horoscopes-network -d -p 9000:9000 meugeninua.horoscopes:1.0

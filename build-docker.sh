0799888144bf81a4c3d0120757c5ff654670e1c8
Yevgeniy Malyshev <meugen.82@gmail.com>
2019-12-26 21:53:46 +0200

    Updated build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -7,5 +7,5 @@ sudo docker rm -f meugeninua.horoscopes
 sudo docker rm -f meugeninua.postgres
 sudo docker build -t meugeninua.horoscopes:1.0 --force-rm .
 
-sudo docker container run --name meugeninua.postgres -e POSTGRES_PASSWORD=horoscopes -e POSTGRES_USER=horoscopes -e POSTGRESS_DB=horoscopes -v postgre-data:/var/lib/postgresql/9/main --network horoscopes-network -d postgres:9.6
+sudo docker container run --name meugeninua.postgres --mount "type=bind,src=/var/postgres/data,dst=/var/lib/postgresql/data" --network horoscopes-network -d postgres:9.6
 sudo docker container run --name meugeninua.horoscopes --network horoscopes-network -d -p 9000:9000 meugeninua.horoscopes:1.0

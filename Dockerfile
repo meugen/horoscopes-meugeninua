f25f7b14e83dfe2b6ba9171e121802edcb61da98
Yevgeniy Malyshev <meugen.82@gmail.com>
2019-12-26 10:23:02 +0200

    In progress for implementing docker container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM store/oracle/serverjre:8
 
 ADD target/universal/horoscopes-meugeninua-1.0-SNAPSHOT.txz /opt
+EXPOSE 9000
 CMD ["/opt/horoscopes-meugeninua-1.0-SNAPSHOT/bin/horoscopes-meugeninua"]
--- build-docker.sh
+++ build-docker.sh
@@ -3,4 +3,9 @@
 sbt clean universal:packageXzTarball
 [ $? -eq 0 ] || exit 1
 sudo docker login
-sudo docker build -t meugeninua.horoscopes --force-rm .
+sudo docker rm -f meugeninua.horoscopes
+sudo docker rm -f meugeninua.postgres
+sudo docker build -t meugeninua.horoscopes:1.0 --force-rm .
+
+sudo docker container run --name meugeninua.postgres -e POSTGRES_PASSWORD=horoscopes -e POSTGRES_USER=horoscopes -e POSTGRESS_DB=horoscopes -v postgre-data:/var/lib/postgresql/9/main --network horoscopes-network -d postgres:9.6
+sudo docker container run --name meugeninua.horoscopes --network horoscopes-network -d -p 9000:9000 meugeninua.horoscopes:1.0

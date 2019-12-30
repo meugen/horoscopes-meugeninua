e4577991221849e1ae13d671659ad88eef7da894
Yevgeniy Malyshev <meugen.82@gmail.com>
2019-12-30 11:57:04 +0200

    Implemented docker-compose.yml

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM store/oracle/serverjre:8
 
 ADD target/universal/horoscopes-meugeninua-1.0-SNAPSHOT.txz /opt
 EXPOSE 9000
-CMD ["/opt/horoscopes-meugeninua-1.0-SNAPSHOT/bin/horoscopes-meugeninua"]
+CMD /opt/horoscopes-meugeninua-1.0-SNAPSHOT/bin/horoscopes-meugeninua
--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POSTGRES_DATA_DIR=/var/postgres/data
+export POSTGRES_DATA_DIR=/var/postgres/data
 sudo mkdir -p $POSTGRES_DATA_DIR
-PGADMIN_SERVERS_JSON=/var/postgres/servers.json
+export PGADMIN_SERVERS_JSON=/var/postgres/servers.json
 if [ ! -f "$PGADMIN_SERVERS_JSON" ]; then
   echo "$PGADMIN_SERVERS_JSON does not exists"
   exit 1
@@ -10,10 +10,11 @@ fi
 
 sbt clean universal:packageXzTarball
 [ $? -eq 0 ] || exit 1
-sudo docker login
-sudo docker rm -f meugeninua.horoscopes meugeninua.postgres meugeninua.pgadmin
-sudo docker build -t meugeninua.horoscopes:1.0 --force-rm .
+docker login
+docker rm -f meugeninua.horoscopes meugeninua.postgres meugeninua.pgadmin
+docker build -t meugeninua.horoscopes:1.0 --force-rm .
 
-sudo docker container run --name meugeninua.postgres -e POSTGRES_PASSWORD=horoscopes -e POSTGRES_USER=horoscopes -e POSTGRES_DB=horoscopes --mount "type=bind,src=$POSTGRES_DATA_DIR,dst=/var/lib/postgresql/data" --network horoscopes-network -d postgres:9.6
-sudo docker container run --name meugeninua.horoscopes --network horoscopes-network -d -p 9000:9000 meugeninua.horoscopes:1.0
-sudo docker container run --name meugeninua.pgadmin -e PGADMIN_DEFAULT_EMAIL=pgadmin@example.com -e PGADMIN_DEFAULT_PASSWORD=welc0me -e PGADMIN_LISTEN_PORT=5480 -v $PGADMIN_SERVERS_JSON:/pgadmin4/servers.json --network horoscopes-network -d -p 5480:5480 dpage/pgadmin4:latest
+# docker container run --name meugeninua.postgres -e POSTGRES_PASSWORD=horoscopes -e POSTGRES_USER=horoscopes -e POSTGRES_DB=horoscopes --mount "type=bind,src=$POSTGRES_DATA_DIR,dst=/var/lib/postgresql/data" --network horoscopes-network -d postgres:9.6
+# docker container run --name meugeninua.horoscopes --network horoscopes-network -d -p 9000:9000 meugeninua.horoscopes:1.0
+# docker container run --name meugeninua.pgadmin -e PGADMIN_DEFAULT_EMAIL=pgadmin@example.com -e PGADMIN_DEFAULT_PASSWORD=welc0me -e PGADMIN_LISTEN_PORT=5480 -v $PGADMIN_SERVERS_JSON:/pgadmin4/servers.json --network horoscopes-network -d -p 5480:5480 dpage/pgadmin4:latest
+docker-compose up -d
